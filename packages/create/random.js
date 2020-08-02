8af8b4e9d9511ce386c874321aee1c506813a4ce
Alex Bechmann <alex.bechmann@outlook.com>
2020-08-03 01:41:04 +0200

    unused file

--- packages/create/random.js
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-const dirname =
-  "/Users/alexbechmann/Documents/development/github/refract-cms/packages/create/pkg/dist-node";
-
-console.log(path.resolve(dirname, "../", "assets"));
