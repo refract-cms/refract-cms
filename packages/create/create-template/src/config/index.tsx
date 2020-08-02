f5e4638983d96b809100846a3a0055f10b622a1a
Alex Bechmann <alex.bechmann@outlook.com>
2020-08-03 00:36:07 +0200

    remove from previous copy

--- packages/create/create-template/src/config/index.tsx
+++
@@ -1,7 +0,0 @@
-// import { configure } from '@refract-cms/core';
-import { configure } from '../../../../packages/core';
-import { ArticleSchema } from './schemas/article';
-
-export const config = configure({
-  schema: [ArticleSchema],
-});
