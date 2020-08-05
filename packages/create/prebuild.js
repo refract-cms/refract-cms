f59379dbce24bbb7f2118f7cd821476c1f38319f
Alex Bechmann <alex.bechmann@outlook.com>
2020-08-05 11:40:24 +0200

    tempate location

--- packages/create/prebuild.js
+++ packages/create/prebuild.js
@@ -2,8 +2,10 @@ const fs = require("fs-extra");
 const path = require("path");
 
 fs.copySync(
-  path.resolve(__dirname, "../create-template"),
-  path.resolve(__dirname, "assets/create-template")
+  path.resolve(__dirname, "../create-template-razzle"),
+  path.resolve(__dirname, "assets/create-template-razzle")
 );
 
-fs.removeSync(path.resolve(__dirname, "assets/create-template/node_modules"));
+fs.removeSync(
+  path.resolve(__dirname, "assets/create-template-razzle/node_modules")
+);
