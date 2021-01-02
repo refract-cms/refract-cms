bd730db44694b9c09e772dbf59619865a94d7d5c
Alex Bechmann <alex.bechmann@outlook.com>
2021-01-02 18:18:24 +0100

    add plugin template

---
+++ packages/plugin-template/rollup.config.js
@@ -0,0 +1,7 @@
+export default {
+  input: ["index.ts", "server.ts"],
+  output: {
+    dir: "./dist",
+    format: "cjs",
+  },
+};
---
+++ packages/plugin-template/server.ts
@@ -0,0 +1,3 @@
+export function go() {
+  console.log("server");
+}
