30ac76a7c659d03733cb8f77e61ec0218dae9c53
Alex <alex.bechmann@outlook.com>
2021-02-12 13:04:34 +0100

    add pulumi project

---
+++ iac/pulumi/index.ts
@@ -0,0 +1,13 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as digitalocean from '@pulumi/digitalocean';
+
+const domain = new digitalocean.Domain('domain', {
+  name: 'refract-cms.com',
+  ipAddress: '185.15.72.81',
+});
+
+new digitalocean.DnsRecord('dns', {
+  value: '*',
+  domain: domain.name,
+  type: 'A',
+});
