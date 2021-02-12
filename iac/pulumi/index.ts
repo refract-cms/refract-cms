67396af1c401f6396c506398b277e5e5407d114a
Alex Bechmann <alex.bechmann@danfoss.com>
2021-02-12 13:49:27 +0100

    update dns records

--- iac/pulumi/index.ts
+++ iac/pulumi/index.ts
@@ -1,14 +1,25 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as digitalocean from '@pulumi/digitalocean';
 
+const ipAddress = '185.15.72.81';
+
 const domain = new digitalocean.Domain('domain', {
   name: 'refract-cms.com',
-  ipAddress: '185.15.72.81',
+  ipAddress,
 });
 
 new digitalocean.DnsRecord('wildcard', {
-  name: 'wildard',
-  value: '*',
   domain: domain.name,
+  ttl: 300,
   type: 'A',
+  name: '*',
+  value: ipAddress,
+});
+
+new digitalocean.DnsRecord('root', {
+  domain: domain.name,
+  ttl: 300,
+  type: 'A',
+  name: '@',
+  value: ipAddress,
 });
